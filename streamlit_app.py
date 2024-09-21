56779996c26a873443140c877ff012247b70685f
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-21 21:45:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit as st
 
 st.title("Hello There, 121192051812913419")
+
+import matplotlib.pyplot as plt
+fig,ax=plt.subplots()
+ax.plot([1,2,3],[3,2,1])
+st.pyplot(fig)
+st.help(st.pyplot)
