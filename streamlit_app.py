dfc0ef5bbee30e713e0b4c348b44bd87bc75ca1b
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-29 21:51:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,4 @@
 import streamlit as st
-from sm_api import last_price
-from datetime import datetime
 
-if 'logs' not in st.session_state:
-    st.session_state['logs']=[]
-
-symbol=st.text_input('Symbol', value="SBIN").upper()
-no_of_shares=st.number_input('No.of Shares', min_value=1, step=1)
-
-
-def buy():
-    global symbol
-    symbol=symbol.upper()
-    price=last_price(symbol)
-    data=(datetime.now().__str__(),symbol, price,no_of_shares)
-    st.session_state['logs'].append(data)
-
-check_price_btn=st.button('Check Price')
-buy_btn=st.button('Buy', on_click=buy)
-st.table(st.session_state['logs'])
+pages=st.navigation([st.Page('buy.py',title='Buy'),st.Page('sell.py',title="Sell"),st.Page('t1.py',title='T1')])
+pages.run()
