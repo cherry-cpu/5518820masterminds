9a0a551d8263fab3d0fd2b94610dc12c233cfc62
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-29 22:54:54 +0530

    Update buy.py

--- buy.py
+++ buy.py
@@ -4,20 +4,26 @@ from datetime import datetime
 import pandas as pd
 
 if 'logs' not in st.session_state:
-    st.session_state['logs']=[('2024-09-29 20:52:32.632811', 'SBIN', 802.3, 1, 802.3), ('2024-09-29 20:52:32.903548', 'SBIN', 802.3, 1, 802.3), ('2024-09-29 20:52:35.073048', 'SBIN', 802.3, 1, 802.3), ('2024-09-29 20:52:42.143048', 'SBIN', 802.3, 1, 802.3)]      
+    st.session_state['logs']=[]
 
 symbol=st.text_input('Symbol', value="SBIN").upper()
 no_of_shares=st.number_input('No.of Shares', min_value=1, step=1)
+
+def timestamp_to_id(timestamp, symbol, nos):
+    return '_'.join(timestamp.replace('-','_').split(':')).split('.')[0]+'_'+'_'.join(timestamp.replace('-','_').split(':')).split('.')[1][:2]+'_'+symbol+'_'+f'{nos}'
 
 def buy():
     global symbol
     symbol=symbol.upper()
     price=last_price(symbol)
     total_price=price*no_of_shares
-    data=(datetime.now().__str__(),symbol, price,no_of_shares, total_price)
+    time=datetime.now().__str__()
+    buy_id=timestamp_to_id(time, symbol, no_of_shares)
+    data=(buy_id,time,symbol, price,no_of_shares, total_price)
     st.session_state['logs'].append(data)
     print(st.session_state['logs'])
+
 check_price_btn=st.button('Check Price')
 buy_btn=st.button('Buy', on_click=buy)
-df=pd.DataFrame(st.session_state['logs'],columns=['Time Stamp','Stock','Price','No.of Shares',"Total Price"])
+df=pd.DataFrame(st.session_state['logs'],columns=['Buy Id','Time Stamp','Stock','Price','No.of Shares',"Total Price"])
 st.dataframe(df, hide_index=True)
