2a0e4dd164d97b6091aa8c644b12105e402950ef
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-29 22:18:41 +0530

    Update sell.py

--- sell.py
+++ sell.py
@@ -2,6 +2,7 @@ import streamlit as st
 from sm_api import last_price
 from datetime import datetime
 import pandas as pd
+from time import sleep
 
 df=pd.DataFrame(st.session_state['logs'],columns=['Time Stamp','Stock','Price','No.of Shares','Total Price'])
 
@@ -13,13 +14,19 @@ def asd():
         print('asdasd')
     st.button('asdasd', on_click=asdasd)
 
+if 'aa' not in st.session_state:
+    st.session_state['aa']=0
 st.button('Asd', on_click=asd)
 def asdasd(i):
     print(f'asdasd {i}')
 
 for i in st.session_state['logs']:
     print(i)
-    with st.popover(i[1]):
+    with st.popover(f"{i[0]}--------{i[1]}----{i[2]}---{i[3]}"):
         st.write(i)
         st.number_input('Sell', key=f'sell_inp_{i[0]}')
         st.button('Sell', key=f'sell_btn_{i[0]}')
+        st.metric(i[1], st.session_state['aa'])
+st.session_state['aa']+=1
+sleep(2)
+st.rerun()
