a1bf9951b4816333fbac34671e97b166070008a5
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-29 13:21:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,50 +1,19 @@
 import streamlit as st
-from random import random
-from time import sleep
-from threading import Thread
-import requests
-from streamlit.components.v1 import html
+from sm_api import last_price
+from datetime import datetime
 
-st.text_input('asda')
+if 'logs' not in st.session_state:
+    st.session_state['logs']=[]
 
-if not st.session_state.get('data'):
-    st.session_state['data'] = []
-data = st.session_state['data']
-data.append(random())
-if len(data) > 30:
-    data = data[-30:]
-st.session_state['data'] = data
-st.line_chart(data)
-if st.toggle("Show message"):
-    st.write("Meggase")
-d=['a','b','c']
-dd=[]
-def asd():
-    global dd
-    r=requests.get('https://meowfacts.herokuapp.com/')
-    dd.append(r.json())
-    return r.json()
-for i in d:
-    st.write(i)
-t=[]
-for j in range(50):
-    t.append(Thread(target=asd))
-for k in t:
-    k.start()
-for asd in t:
-    asd.join()
+symbol=st.text_input('Symbol', value="SBIN")
+no_of_shares=st.number_input('No.of Shares', min_value=1, step=1)
 
-st.write(dd)
-print(dd)
-st.text_input('asd')
-st.markdown(
-"""
-<style>
-#root > div:nth-child(1) > div.withScreencast > div > div > header > div.st-emotion-cache-15ecox0.ezrtsby0 > div.st-emotion-cache-19or5k2.en6cib61.StatusWidget-enter-done > div > img{
-display:none;}
-</style>
-""",
-    unsafe_allow_html=True,
-)
-sleep(5)
-st.rerun()
+
+def buy():
+    price=last_price(symbol)
+    data=(datetime.now().__str__(),symbol, price,no_of_shares)
+    st.session_state['logs'].append(data)
+
+check_price_btn=st.button('Check Price')
+buy_btn=st.button('Buy', on_click=buy)
+st.table(st.session_state['logs'])
