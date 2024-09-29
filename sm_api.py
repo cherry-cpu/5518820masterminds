92a6fa16c83d6a39c5909d6268e5f0d67f502763
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-29 13:22:17 +0530

    Add files via upload

---
+++ sm_api.py
@@ -0,0 +1,13 @@
+import requests as r
+h={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+}
+s=r.Session()
+s.get('https://www.nseindia.com', headers=h)
+
+def last_price(symbol):
+    res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
+    return res.json()['priceInfo']['lastPrice']
