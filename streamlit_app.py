0648577d95e9198399ad9adda028b91b21ebb1b0
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-30 13:09:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,5 @@ import streamlit as st
 
 # https://github.com/cherry-cpu/5518820masterminds/blob/main/buy.py
 
-if 'logs' not in st.session_state:
-    st.session_state['logs']=[]
-if 'sell_data' not in st.session_state:
-    st.session_state['sell_data']=[]
-
 pages=st.navigation([st.Page('buy.py',title='Buy'),st.Page('sell.py',title="Sell"),st.Page('t1.py',title='T1')])
 pages.run()
