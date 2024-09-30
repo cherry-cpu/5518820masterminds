78d9de8803b6ba1d01502f88c25fd1f8a020a2f3
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-09-30 13:33:19 +0530

    Update t1.py

--- t1.py
+++ t1.py
@@ -1,4 +1,15 @@
 import streamlit as st
-from sm_api import last_price
+import requests as r
+h={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+}
+s=r.Session()
+symbol='SBIN'
+s.get('https://www.nseindia.com', headers=h)
+res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
+resè=res.json()['priceInfo']['lastPrice']
 
-st.write(last_price('SBIN'))
+st.write(rese)
