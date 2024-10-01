bd23fbeadaf55c273af2fa1f71c2773421065b77
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-10-01 21:40:46 +0530

    Update sm_api.py

--- sm_api.py
+++ sm_api.py
@@ -1,15 +1,21 @@
 import requests as r
-h={
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
-    'Accept-Encoding': 'gzip, deflate',
-    'Accept': '*/*',
-    'Connection': 'keep-alive',
-}
+#h={
+#    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+#    'Accept-Encoding': 'gzip, deflate',
+#    'Accept': '*/*',
+#    'Connection': 'keep-alive',
+#}
 #s=r.Session()
 #s.get('https://www.nseindia.com', headers=h)
 
 def last_price(symbol):
     s=r.Session()
+    h={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+    }
     s.get('https://www.nseindia.com', headers=h)
     res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
 #   res=s.get(f'https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks', headers=h)
