21bb7404032009a120a2beeab39b5ffa6d3a0e62
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-10-01 21:43:07 +0530

    Update sm_api.py

--- sm_api.py
+++ sm_api.py
@@ -9,7 +9,7 @@ s=r.Session()
 s.get('https://www.nseindia.com', headers=h)
 
 def last_price(symbol):
-    global s
+#    global s
 #    s=r.Session()
     h={
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
