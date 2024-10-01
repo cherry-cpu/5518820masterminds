7b6d52ccc1024bbb86109e8abf2f780b4311b149
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-10-01 21:42:43 +0530

    Update sm_api.py

--- sm_api.py
+++ sm_api.py
@@ -5,18 +5,19 @@ import requests as r
 #    'Accept': '*/*',
 #    'Connection': 'keep-alive',
 #}
-#s=r.Session()
-#s.get('https://www.nseindia.com', headers=h)
+s=r.Session()
+s.get('https://www.nseindia.com', headers=h)
 
 def last_price(symbol):
-    s=r.Session()
+    global s
+#    s=r.Session()
     h={
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
     'Accept-Encoding': 'gzip, deflate',
     'Accept': '*/*',
     'Connection': 'keep-alive',
     }
-    s.get('https://www.nseindia.com', headers=h)
+#    s.get('https://www.nseindia.com', headers=h)
     res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
 #   res=s.get(f'https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks', headers=h)
     return res.json()['priceInfo']['lastPrice']
