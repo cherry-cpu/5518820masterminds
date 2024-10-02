2ecd8dfddb1c11fff0a1281ee07f9335f2dc9d8d
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-10-02 07:06:30 +0530

    Update sm_api.py

--- sm_api.py
+++ sm_api.py
@@ -17,9 +17,9 @@ def last_price(symbol):
     'Accept': '*/*',
     'Connection': 'keep-alive',
     }
- #   s.get('https://www.nseindia.com', headers=h)
- #   res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
-    res=s.get(f'https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks', headers=h)
-#    return res.json()['priceInfo']['lastPrice']
+#    s.get('https://www.nseindia.com', headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36', 'Accept-Encoding': 'gzip, deflate','Accept': '*/*','Connection': 'keep-alive', })
+    res=s.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=h)
+#    res=s.get(f'https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks', headers=h)
+    return res.json()['priceInfo']['lastPrice']
 
-    return res.json()['data']['primaryData']['lastSalePrice']
+#    return res.json()['data']['primaryData']['lastSalePrice']
