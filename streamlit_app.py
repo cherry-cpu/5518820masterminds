007083c8025e4b18b2cb422eb09da8cfc5a6d7df
cherry-cpu <73707312+cherry-cpu@users.noreply.github.com>
2024-10-08 22:35:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 # https://github.com/cherry-cpu/5518820masterminds/blob/main/buy.py
 
-pages=st.navigation([st.Page('buy.py',title='Buy'),st.Page('sell.py',title="Sell"),st.Page('t1.py',title='T1')])
+pages=st.navigation([st.Page('user_page.py', title='User'),st.Page('buy.py',title='Buy'),st.Page('sell.py',title="Sell"),st.Page('t1.py',title='T1')])
 pages.run()
